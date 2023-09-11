acccab65be00c2524575335549bd32c4fafc7d9e
ChiragAjmera57 <chiragajmera57@gmail.com>
2023-09-11 20:57:36 +0530

    image upload size

--- Backend/index.js
+++ Backend/index.js
@@ -11,7 +11,8 @@ const Register = require('./model/registerEvent')
 require('dotenv').config() 
 
 app.use(cors())
-app.use(express.json())
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({limit: '50mb'}));
 
 
 // sign up
